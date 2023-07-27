7d48f3b2c357184e62c6a0f7e51f1f6df2096643
Jakub <mroczny322@gmail.com>
2023-07-27 18:10:31 +0200

    section 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+// const bookings = [];
+
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+
+//     const booking = {
+//         flightNum,
+//         numPassengers,
+//         price
+//     }
+//     console.log(booking);
+//     bookings.push(booking);
+// }
+
+// createBooking(`LC16`, 165, 78);
+
+// const flight = `LC16`;
+// const kuba = {
+//     name: `Jakub Marciniak`,
+//     passport: 41337221150
+// }
+
+// const checkIn = function (flightNum, passenger) {
+//     flightNum = `VER1`;
+//     passenger.name = `Mr. ` + passenger.name;
+//     if (passenger.passport === 41337221150) {
+//         alert(`Lecisz`)
+//     } else {
+//         alert(`Nie lecisz`)
+//     }
+// }
+
+// checkIn(flight, kuba);
+// console.log(flight);
+// console.log(kuba);
+
+
+// const newPassport = function (person) {
+//     person.passport = Math.trunc(Math.random() * 10000000000);
+// }
+
+// newPassport(kuba);
+// checkIn(flight, kuba);
+// console.log(kuba);
