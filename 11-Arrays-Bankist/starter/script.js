2125a7a9e08b1f178f948754fc7b9151ec5c400d
Jakub <mroczny322@gmail.com>
2023-08-06 13:31:30 +0200

    11-155

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -414,26 +414,33 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const ages = [5, 2, 4, 1, 15, 8, 3];
-const calcAverageHumanAge = function (ages) {
-  const humanAges = ages.map(function(age){
-    if (age <= 2){
-      const newAge = age * 2;
-      return newAge;
-    } else {
-      const newAge1 = 16 + age * 4;
-      return newAge1;
-    }
-  });
-  const adults = humanAges.filter(function(newAge1){
-    return newAge1 >= 18;
-  });
-  console.log(humanAges);
-  console.log(adults);
+// const ages = [5, 2, 4, 1, 15, 8, 3];
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(function(age){
+//     if (age <= 2){
+//       const newAge = age * 2;
+//       return newAge;
+//     } else {
+//       const newAge1 = 16 + age * 4;
+//       return newAge1;
+//     }
+//   });
+//   const adults = humanAges.filter(function(newAge1){
+//     return newAge1 >= 18;
+//   });
+//   console.log(humanAges);
+//   console.log(adults);
 
-  const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length,0 );
+//   const average = adults.reduce((acc, age, i, arr) => acc + age / arr.length,0 );
 
-  return average;
-};
+//   return average;
+// };
 
-console.log(calcAverageHumanAge(ages));
+// console.log(calcAverageHumanAge(ages));
+
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+.filter(mov => mov > 0)
+.map(mov => mov * eurToUsd)
+.reduce((acc, mov)=> acc + mov, 0);
+console.log(totalDepositsUSD);
