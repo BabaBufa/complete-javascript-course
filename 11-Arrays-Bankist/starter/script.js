7f43ecab3300c09bfc1140bce2ae64f2a2e0bb2d
Jakub <mroczny322@gmail.com>
2023-08-06 16:25:47 +0200

    11-160

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -178,28 +178,15 @@ btnLoan.addEventListener('click', function (e) {
   inputLoanAmount.value = '';
 });
 
-btnClose.addEventListener('click', function (e) {
+btnClose.addEventListener(`click`, function(e){
   e.preventDefault();
-
-  if (
-    inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
-  ) {
-    const index = accounts.findIndex(
-      acc => acc.username === currentAccount.username
-    );
-    console.log(index);
-    // .indexOf(23)
-
-    // Delete account
+  if(inputCloseUsername.value === currentAccount.username && Number(inputClosePin.value) === currentAccount.pin ){
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
     accounts.splice(index, 1);
-
-    // Hide UI
     containerApp.style.opacity = 0;
   }
-
-  inputCloseUsername.value = inputClosePin.value = '';
-});
+  inputCloseUsername.value = inputClosePin.value = ``;
+})
 
 let sorted = false;
 btnSort.addEventListener('click', function (e) {
