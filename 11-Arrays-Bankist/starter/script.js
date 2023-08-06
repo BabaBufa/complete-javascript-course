2d214aa4b522df4167ab930468aa5e073191f70d
Jakub <mroczny322@gmail.com>
2023-08-06 13:56:41 +0200

    11-157

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -457,11 +457,14 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const lata = [5, 2, 4, 1, 15, 8, 3];
-const calcAverageHumanAge = ages =>
-  ages
-    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter(age => age >= 18)
-    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+// const lata = [5, 2, 4, 1, 15, 8, 3];
+// const calcAverageHumanAge = ages =>
+//   ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18)
+//     .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
 
-  console.log(calcAverageHumanAge(lata));
+//   console.log(calcAverageHumanAge(lata));
+
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(firstWithdrawal);
